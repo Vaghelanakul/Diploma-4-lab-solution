51f3fad2364bbe5e79b7fe495949ae0e2af2d6d1
Vaghela Nakul <nakulvaghela11@gmail.com>
2025-11-20 01:19:23 +0530

    perform lab 7 practicals

---
+++ Lab7/practical1.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="get">
+        Enter number:<input type="number" name="num" placeholder="Enter number">
+        <br>
+        <input type="submit" name="submit" value="Submit">
+    </form>
+    <?php
+        if(isset($_GET['submit'])){
+            $num=$_GET['num'];
+            for($i=0;$i<$num;$i++){
+                for($j=0;$j<=$i;$j++){
+                    echo "* ";
+                }
+                echo "<br>";
+            }
+        }
+
+        //153,370,371,407
+    ?>
+</body>
+</html>
