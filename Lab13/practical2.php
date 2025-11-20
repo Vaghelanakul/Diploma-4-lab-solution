2317362286596d1ad038cbc894ec248da4619a06
Vaghela Nakul <nakulvaghela11@gmail.com>
2025-11-21 03:25:38 +0530

    perform lab 13 programs

---
+++ Lab13/practical2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+</head>     
+<body bgcolor="gray">
+    <?php include 'header.php'; ?>
+
+    <h2>This is the main content of the page.</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in dui mauris.</p>
+    <?php include 'content.php'; ?>
+    <?php include 'footer.php'; ?>
+</body>
+</html>
